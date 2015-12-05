09b64a5d108163057e4800a58567984730c55be7
David Hopkins <dhop@apple.com>
2015-12-04 16:43:12 -0800

    Sentry changes

--- swautocheckin/settings/dev.py
+++ swautocheckin/settings/dev.py
@@ -4,6 +4,5 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 INSTALLED_APPS += (
-    'debug_toolbar', # and other apps for local development
+    'debug_toolbar',  # and other apps for local development
 )
-
--- swautocheckin/settings/prod.py
+++ swautocheckin/settings/prod.py
@@ -1,3 +1,4 @@
+import logging
 from swautocheckin.settings.common import *
 import dj_database_url
 
@@ -10,24 +11,36 @@ INSTALLED_APPS += (
 RAVEN_CONFIG = {
     'dsn': os.environ['SENTRY_DSN'],
     # 'release': raven.fetch_git_sha(BASE_DIR),
+    'CELERY_LOGLEVEL': logging.INFO,
 }
 
 SENTRY_AUTO_LOG_STACKS = True
 
 LOGGING['handlers']['sentry'] = {
-    'level': 'DEBUG',
+    'level': 'ERROR',
     'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
 }
 
-LOGGING['loggers']['celery'] = {
-    'level': 'DEBUG',
-    'handlers': ['sentry', 'console'],
-    'propagate': False,
-}
+LOGGING['loggers'].update({
+    'celery': {
+        'level': 'DEBUG',
+        'handlers': ['sentry', 'console'],
+        'propagate': False,
+    },
+
+    'raven': {
+        'level': 'DEBUG',
+        'handlers': ['console'],
+        'propagate': False,
+    },
+    'sentry.errors': {
+        'level': 'DEBUG',
+        'handlers': ['console'],
+        'propagate': False,
+    }
+})
 
 # Parse database configuration from $DATABASE_URL
-DATABASES['default'] = dj_database_url.config()
-
-# Redis config
-BROKER_URL = os.environ['REDIS_URL'],
+DATABASES['default'] = dj_database_url.config()  # Redis config
+BROKER_URL = os.environ['REDIS_URL']
 CELERY_RESULT_BACKEND = os.environ['REDIS_URL']
